d6b36177a35fc23e30baba4fbb872276ef67bda5
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-05-08 19:08:12 +0200

    fix user model

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
   membership: { type: String, required: true },
 });
