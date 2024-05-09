c40af3ef17846d5a8ee30f122e733e06e16e2fc3
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-05-09 19:49:12 +0200

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../controllers/userController");
+const messageController = require("../controllers/messageController");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get("/", messageController.index);
+router.get("/sign_up", userController.sign_up_get);
+router.post("/sign_up", userController.sign_up_post);
+router.get("/join_club", userController.join_club_get);
+router.post("/join_club", userController.join_club_post);
+router.get("/log_in", userController.log_in_get);
+router.post("/log_in", userController.log_in_post);
+router.get("/log_out", userController.log_out);
+router.post("/send_message", messageController.send_message);
 
 module.exports = router;
