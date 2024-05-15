6cd92c3d915efe3985c33cc7ed7fcc45163cde5c
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-05-15 11:24:06 +0200

    remove comments

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 const User = require("./models/user");
+
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(process.env.db);
@@ -71,18 +73,14 @@ app.use((req, res, next) => {
 });
 app.use("/", indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
